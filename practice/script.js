607c21488f508e898d94a73204ed4007193505cd
D-diary <dlrlsud@naver.com>
2022-04-17 01:45:28 +0900

    3-5. API 연동 - 데이터 삭제하기

--- practice/script.js
+++ practice/script.js
@@ -143,6 +143,16 @@
     .catch(error => console.error(error))
   }
 
+  const removeTodo = (e) => {
+    if(e.target.className !== 'todo_remove_button') return
+    const $item = e.target.closest('.item')
+    const id = $item.dataset.id
+
+    fetch(`${API_URL}/${id}`, { method: 'DELETE'})
+      .then(getTodos)
+      .catch(error => console.error(error))
+  }
+
   const init = () => {
     window.addEventListener('DOMContentLoaded', () => {
       getTodos();
@@ -151,6 +161,7 @@
     $todos.addEventListener('click', toggleTodo)
     $todos.addEventListener('click', changeEditMode)
     $todos.addEventListener('click', editTodo)
+    $todos.addEventListener('click', removeTodo)
   }
   init()
 })()
